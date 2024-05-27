801d25043ee82bf6c76ae35af19aaa9381d0f660
Ingo Ruhnke <grumbel@gmail.com>
2024-05-27 20:18:34 +0200

    Update to SDL2_image 2.8.2 and nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
 
-    SDL2_image_src.url = "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-mingw.tar.gz";
+    SDL2_image_src.url = "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.8.2-mingw.tar.gz";
     SDL2_image_src.flake = false;
   };
 
@@ -17,7 +17,7 @@
 
           SDL2_image = pkgs.stdenv.mkDerivation rec {
             pname = "SDL2_image";
-            version = "2.0.5";
+            version = "2.8.2";
 
             src = SDL2_image_src;
 
